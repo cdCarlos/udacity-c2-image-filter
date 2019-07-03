b393a9332bf7b0d507886f3a9a42301dc72a2352
Carlos Daniel <cd.carlosx2@gmail.com>
2019-07-02 19:56:58 -0600

    Modularize app router

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Request, Response } from "express";
-import {
-  filterImageFromURL,
-  deleteLocalFiles,
-  isImageTypeSupported,
-  validateURL
-} from "./util/util";
+import { IndexRouter } from "./controllers/v0/index.router";
 
 (async () => {
   // Init the Express application
@@ -18,49 +12,11 @@ import {
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage", async (req: Request, res: Response) => {
-    let imageURL: string = req.query.image_url;
+  app.use("/api/v0/", IndexRouter);
 
-    if (!imageURL) {
-      return res
-        .status(400)
-        .send({ message: "Image URL required or malformed" });
-    }
-
-    if (!isImageTypeSupported(imageURL.toLowerCase())) {
-      return res.status(400).send({ message: "Image type not supported" });
-    }
-
-    imageURL = validateURL(imageURL);
-    if (!imageURL) {
-      return res.status(400).send({ message: "Image URL malformed" });
-    }
-
-    let filteredImagePath: string;
-    try {
-      filteredImagePath = await filterImageFromURL(imageURL);
-    } catch (err) {
-      console.error("ERROR::applyFilter >> ", err);
-      return res.status(204).send({ message: "Error while filtering image" });
-    }
-
-    res.download(filteredImagePath, async err => {
-      if (err) {
-        res.status(204).end();
-      }
-
-      try {
-        await deleteLocalFiles([filteredImagePath]);
-      } catch (err) {
-        console.error("ERROR::deleteTempFiles >> ", err);
-      }
-    });
-  });
-
-  // Root Endpoint
-  // Displays a simple message to the user
+  // Root URI call
   app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}");
+    res.send("/api/v0/");
   });
 
   // Start the Server
