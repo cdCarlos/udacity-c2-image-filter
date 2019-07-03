90d5895aa954acb1d3ededca670e2e3afc0ecc15
Carlos Daniel <cd.carlosx2@gmail.com>
2019-07-02 19:35:02 -0600

    Refactor variables name into camel-case format

--- src/util/util.ts
+++ src/util/util.ts
@@ -13,14 +13,14 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
       const photo = await Jimp.read(inputURL);
-      const outpath = `/tmp/filtered.${Math.floor(Math.random() * 2000)}.jpg`;
+      const outPath = `/tmp/filtered.${Math.floor(Math.random() * 2000)}.jpg`;
 
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, img => {
-          resolve(__dirname + outpath);
+        .write(__dirname + outPath, img => {
+          resolve(__dirname + outPath);
         });
     } catch (err) {
       console.error("ERROR::filterImageFromURL >> ", err);
