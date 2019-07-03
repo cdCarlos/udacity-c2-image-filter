eff83bebebd130bf99ec0fb1adeb6a5ada7be2be
Carlos Daniel <cd.carlosx2@gmail.com>
2019-07-02 19:35:42 -0600

    Refactor documentation for original helper functions

--- src/util/util.ts
+++ src/util/util.ts
@@ -2,13 +2,14 @@ import fs from "fs";
 import Jimp = require("jimp");
 const URL = require("url-parse");
 
-// filterImageFromURL
-// helper function to download, filter, and save the filtered image locally
-// returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
-// RETURNS
-//    an absolute path to a filtered image locally saved file
+/**
+ * Helper function to download, filter, and save the filtered image locally.
+ *
+ * @param {String} inputURL A publicly accessible url to an image file.
+ *
+ * @returns {String} Absolute path to a filtered image and locally saved file.
+ *
+ */
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
@@ -29,11 +30,12 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
   });
 }
 
-// deleteLocalFiles
-// helper function to delete files on the local disk
-// useful to cleanup after tasks
-// INPUTS
-//    files: Array<string> an array of absolute paths to files
+/**
+ * helper function to delete files on the local disk, useful to cleanup after applying filters to images.
+ *
+ * @param {Array<String>} files An array of absolute paths to files.
+ *
+ */
 export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
     fs.unlinkSync(file);
