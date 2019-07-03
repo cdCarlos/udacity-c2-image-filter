ab4531e4b19782f34df8b96f32ed91b29cad755d
Carlos Daniel <cd.carlosx2@gmail.com>
2019-07-02 18:52:19 -0600

    Endpoint to filter image
    Image URL is passed as parameter, if image is in upported formats images
    is resized, wuality set to 60 dpi and a gray scale filter
    is added to finaly download to the client.

--- src/server.ts
+++ src/server.ts
@@ -13,23 +13,48 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const { image_url } = req.query;
 
-  /**************************************************************************** */
+    if (!image_url) {
+      return res
+        .status(400)
+        .send({ message: "Image URL required or malformed" });
+    }
 
-  //! END @TODO1
+    if (!isImageTypeSupported(image_url.toLowerCase())) {
+      return res.status(400).send({ message: "Image type not supported" });
+    }
+
+    let url: string;
+
+    try {
+      url = new URL(image_url);
+    } catch (e) {
+      console.error("ERROR::parseURL >> ", e);
+      return res.status(400).send({ message: "Malformed image URL" });
+    }
+
+    let filtered_image: string;
+    try {
+      filtered_image = await filterImageFromURL(url.toString());
+    } catch (err) {
+      console.error("ERROR::applyFilter >> ", err);
+      return res.status(204).send({ message: "Error while filtering image" });
+    }
+
+    res.download(filtered_image, async err => {
+      if (err) {
+        res.status(204).end();
+      }
+
+      try {
+        await deleteLocalFiles([filtered_image]);
+      } catch (err) {
+        console.error("ERROR::deleteTempFiles >> ", err);
+      }
+    });
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
